4a45b389ccccabd9c70e4ef9fa3d3852b298c22a
joshrashtian <joshrashtian1@gmail.com>
2024-04-08 21:18:30 -0700

    Work on EventCreator

--- app/connect/Event/CreateEvent/index.tsx
+++ app/connect/Event/CreateEvent/index.tsx
@@ -1,12 +1,39 @@
-import { StyleSheet, Text, View, TextInput, FlatList } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  View,
+  TextInput,
+  FlatList,
+  TouchableOpacity,
+} from "react-native";
 import React, { useState } from "react";
-import { type EventType } from "../../../(components)/EventComponent";
+import { months, type EventType } from "../../../(components)/EventComponent";
 import FormContainer from "./FormContainer";
 import FormInput from "../../../(components)/Components/Input";
 import { KeyboardAwareScrollView } from "react-native-keyboard-aware-scroll-view";
 import DateTimePicker from "@react-native-community/datetimepicker";
 import DatePicker from "./DatePicker";
+import { useUser } from "../../../(contexts)/AuthContext";
+
+const types = [
+  {
+    type: "User Created",
+  },
+  {
+    type: "Workshop",
+  },
+  {
+    type: "Official MESA",
+    permissions: ["admin"],
+  },
+  {
+    type: "Tutoring",
+    permissions: ["admin", "tutor", "moderator"],
+  },
+];
+
 const CreateEvent = () => {
+  const { data } = useUser();
   const [eventDetails, setEventDetails] = useState<EventType>();
   const [date, setDate] = useState<{
     startDate: Date;
@@ -51,17 +78,66 @@ const CreateEvent = () => {
           console.log("recieved", e);
         }}
       />
-      <Text>
-        {date?.startDate?.toLocaleString()} {date?.endDate?.toLocaleString()}
+      <Text
+        style={{
+          fontFamily: "eudoxus",
+          fontSize: 24,
+          color: "#830",
+        }}
+      >
+        {date?.startDate &&
+          `On ${
+            months[date.startDate.getMonth()]
+          } ${date.startDate.getDate()}, at ${date.startDate.getHours()}:${
+            date.startDate.getMinutes() < 10
+              ? `0${date.startDate.getMinutes()}`
+              : date.startDate.getMinutes()
+          } `}{" "}
+        {date?.endDate &&
+          `and ending at ${
+            months[date.endDate.getMonth()]
+          } ${date.endDate.getDate()}, at ${date.endDate.getHours()}:${
+            date.endDate.getMinutes() < 10
+              ? `0${date.endDate.getMinutes()}`
+              : date.endDate.getMinutes()
+          }`}
+        .
+      </Text>
+      <Text
+        style={{
+          fontFamily: "mono",
+          fontSize: 28,
+          marginTop: 30,
+          marginBottom: 10,
+        }}
+      >
+        Types
       </Text>
       <FlatList
-        data={["Personal"]}
+        data={types}
         horizontal
-        renderItem={({ item }) => (
-          <View>
-            <Text>{item}</Text>
-          </View>
-        )}
+        renderItem={({ item }) => {
+          //@ts-ignore
+          if (item.permissions && !item.permissions.includes(data.role))
+            return null;
+
+          return (
+            <TouchableOpacity
+              onPress={(e) => {
+                // @ts-ignore
+                setEventDetails({ ...eventDetails, type: e });
+              }}
+              style={{
+                padding: 10,
+                marginHorizontal: 4,
+                borderRadius: 10,
+                backgroundColor: "#E23C1D",
+              }}
+            >
+              <Text style={{ color: "#fff" }}>{item.type}</Text>
+            </TouchableOpacity>
+          );
+        }}
       />
     </KeyboardAwareScrollView>
   );
