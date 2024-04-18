98eed51a5fbff7b74b4e1cb2f7268e927916deac
joshrashtian <joshrashtian1@gmail.com>
2024-04-18 14:37:41 -0700

    event search improved

--- app/connect/Social/Search/search.ts
+++ app/connect/Social/Search/search.ts
@@ -4,14 +4,17 @@ export const Search = async (type: string, query: string) => {
   const { data, error } = await supabase
     .from(type)
     .select("*")
-    .textSearch(type === "posts" ? "title_tags_creator" : "name", query, {
-      type: "websearch",
-    });
+    .textSearch(
+      type === "posts" ? "title_tags_creator" : "event_search",
+      query,
+      {
+        type: "websearch",
+      }
+    );
 
   if (error) {
     console.log(error);
   }
 
-  console.log(data);
   return { data, error };
 };
