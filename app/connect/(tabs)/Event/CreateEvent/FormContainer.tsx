0ab4e838ac63c7090db91d15e855418dae8eee0e
joshrashtian <joshrashtian1@gmail.com>
2024-06-27 16:06:48 -0700

    fix 2

--- app/connect/(tabs)/Event/CreateEvent/FormContainer.tsx
+++
@@ -1,8 +0,0 @@
-import { View, Text, KeyboardAvoidingView } from "react-native";
-import React from "react";
-import { KeyboardAwareScrollView } from "react-native-keyboard-aware-scroll-view";
-const FormContainer = (props: any) => {
-  return <KeyboardAwareScrollView {...props} >{props.children}</KeyboardAwareScrollView>;
-};
-
-export default FormContainer;
